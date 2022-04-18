e302938390dc5cfc2cf2e4907ca24b31a39d3da1
santhosh-it2022 <santhosh.it2022@gmail.com>
2022-04-18 12:42:16 +0530

    springboot changes

---
+++ src/test/java/com/persistent/DockerDemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistent.DockerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
